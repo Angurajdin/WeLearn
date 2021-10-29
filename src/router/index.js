41db27b2795a3035d2a5dc3d2f6499194f25d949
Angurajdin <angurajdineshb@gmail.com>
2021-10-29 15:14:28 +0530

    auth individual

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,62 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: '/Home',
-    name: 'Home',
+    path: "/Home",
+    name: "Home",
     component: Home,
-    children:[
+    children: [
       {
-        path:'/',
-        name:'Welearn',
-        component: () => import('../views/Welearn.vue')
-
-      },
-      
-     
-      {
-        path: '/Explore',
-        name: 'Explore',
-        component: () => import('../views/Explore.vue')
+        path: "/",
+        name: "Welearn",
+        component: () => import("../views/Welearn.vue"),
       },
 
       {
-        path: '/Webinar',
-        name: 'Webinar',
-        component: () => import('../views/Webinar.vue')
+        path: "/Explore",
+        name: "Explore",
+        component: () => import("../views/Explore.vue"),
       },
 
       {
-        path: '/Discussion',
-        name: 'Discussion',
-        component: () => import('../views/Discussion.vue')
+        path: "/Webinar",
+        name: "Webinar",
+        component: () => import("../views/Webinar.vue"),
+      },
+
+      {
+        path: "/Discussion",
+        name: "Discussion",
+        component: () => import("../views/Discussion.vue"),
       },
       {
-        path: '/Questions',
-        name: 'Qa',
-        component: () => import('../views/Qa.vue')
+        path: "/Questions",
+        name: "Qa",
+        component: () => import("../views/Qa.vue"),
       },
-    ]
-   
+    ],
   },
   {
-    path:"/Content",
-    name:"Content",
-    component:() => import('../views/Content.vue')
-  }
-]
+    path: "/Content",
+    name: "Content",
+    component: () => import("../views/Content.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/login.vue"),
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: () => import("../views/signup.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/signup.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="row">
+        <div class="col-4"></div>
+            <div class="col-4">
+                <center><h1>Signup</h1></center><br>
+                <div class="mb-3">
+                    <input v-model="userName" type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <input v-model="emailid" type="email" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <input v-model="password" type="password" class="form-control">
+                </div>
+                <center>
+                    <button @click="onSubmit" type="submit" class="btn btn-primary">Submit</button>
+                </center>
+        </div>
+        <div class="col-4"></div>
+        
+    </div>
+
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import API from '../connections/api'
+
+export default defineComponent({
+
+    setup(){
+        let emailid = ref("");
+        let password = ref("");
+        let userName = ref("");
+        const router = useRouter()
+        const onSubmit = async() => {
+            const a = await API.signup({
+                userName: userName.value,
+                emailID: emailid.value,
+                password: password.value
+            })
+            console.log(a);
+        }
+
+        return { userName, emailid, password, onSubmit }
+    }
+
+});
+
+</script>
+
+<style>
+
+</style>
